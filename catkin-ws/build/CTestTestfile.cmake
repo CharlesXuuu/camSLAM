d9bdc1fa5705496953ec29f896f5d4e98f26e2bc
root <CharlesXuuu>
2016-11-03 22:11:00 -0700

    update

---
+++ catkin-ws/build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/chix/catkin-ws/src
+# Build directory: /home/chix/catkin-ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+SUBDIRS(gtest)
+SUBDIRS(usb_cam)
+SUBDIRS(video_stream_opencv)
